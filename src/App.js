6046b50167a13cc996d280ad2fb30ba53e25e1a7
workvictor <workvictor@yandex.ru>
2018-05-23 00:33:57 +0300

    add store provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React        from 'react';
+import { Provider } from 'react-redux';
+import { store }    from './store';
+import {
+  actions,
+  types
+}                   from './store';
 
 
-class App extends Component {
-  render() {
+class App extends React.Component{
+  render(){
     return (
-      <div>
-        initial
-      </div>
+      <Provider store={store}>
+        <div>
+          test
+        </div>
+      </Provider>
     );
   }
 }
