bd371f059c70b5c88cadf691c743ec1bd37e372a
workvictor <workvictor@yandex.ru>
2018-05-22 20:51:33 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        initial
+      </div>
+    );
+  }
+}
+
+export default App;
