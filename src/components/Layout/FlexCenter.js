7a7f9c02644b6a53c8cd47cd168ff872c35e2217
workvictor <workvictor@yandex.ru>
2018-05-26 00:52:21 +0300

    add layout component

---
+++ src/components/Layout/FlexCenter.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const FlexCenter=styled.div`
+	text-align: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	align-content: center;
+	flex-direction: column;
+	position: relative;
+	width: 100%;
+	height: 100%;
+`;
