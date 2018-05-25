1964d882ad3568dd35c9e4ec255e75f2b0aea497
workvictor <workvictor@yandex.ru>
2018-05-26 00:52:03 +0300

    add animated transition

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,30 @@
-import React  from 'react';
-import styled from 'styled-components';
+import React          from 'react';
+import styled         from 'styled-components';
+import { Animated }   from '../Animated';
+import { FlexCenter } from '../Layout';
 
 
-const Wrapper=styled.div`
-	text-align: center;
+const Wrapper=styled(FlexCenter)`
+	& h1{
+		margin-bottom: 24px;
+	}
+	& .text-shadow{
+		text-shadow: 0 2px 6px #000;
+	}
 `;
 
-export const Home=()=>{
-	return (
-		<Wrapper>
-			<h1>Home</h1>
-		</Wrapper>
-	);
-};
+
+export class Home extends React.Component{
+	render(){
+		return (
+			<Wrapper>
+				<Animated animation={`fadeInDown`}>
+					<h1 className={`text-shadow`}>Hello</h1>
+				</Animated>
+				<Animated animation={`fadeInDown`}>
+					<span className={`text-shadow`}>Start your chat right now for free</span>
+				</Animated>
+			</Wrapper>
+		);
+	}
+}
