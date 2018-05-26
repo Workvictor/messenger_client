1a84a99a70f39e7ea4ef59b7e9f6b34db4abbf1d
workvictor <workvictor@yandex.ru>
2018-05-26 11:50:41 +0300

    update toolbar

--- src/components/MainWrapper/ToolBar.js
+++ src/components/MainWrapper/ToolBar.js
@@ -19,10 +19,6 @@ const Wrapper=styled.div`
 	flex-direction: column;
 `;
 
-const StartHint=styled.div`	
-	font-size: 11px;
-	padding: 4px 24px;
-`;
 const NavBar=styled.div`
 	user-select: none;
 	text-align: left;
@@ -52,16 +48,18 @@ const NavBar=styled.div`
 `;
 
 const BarRow=styled.div`	
-	position: absolute;
+	position: relative;
 	top: 0;
 	left: 0;	
 	display: flex;
 	align-items: center;
 	justify-content: start;
 	width: 100%;
+	height: 40px;
 	transition: all 300ms;
 	background-color: ${({ visible })=>visible ? `rgba(37, 35, 64, 0.65)` : `transparent`};
 `;
+
 const BarCol=styled.div`
 	display: flex;
 	align-items: center;
@@ -83,13 +81,11 @@ const BarPoint=styled.div`
 export class ToolBar extends React.Component{
 
 	state={
-		showStartHint: true,
 		visible: false,
 	};
 
 	onMouseEnter=()=>{
 		this.setState({
-			showStartHint: false,
 			visible: true,
 		});
 	};
@@ -102,29 +98,27 @@ export class ToolBar extends React.Component{
 
 	render(){
 		return (
-			<Wrapper
+			<BarRow
+				visible={this.state.visible}
 				onMouseEnter={this.onMouseEnter}
 				onMouseLeave={this.onMouseLeave}
 			>
-				<BarRow visible={this.state.visible}>
-					<BarCol>
-						<Flex>
-							<BarPoint color={`#E63E3E`}/>
-							<BarPoint color={`#EFBF14`}/>
-							<BarPoint color={`#38AD59`}/>
-							{this.state.showStartHint && <StartHint>start here</StartHint>}
-						</Flex>
-					</BarCol>
-					<BarCol>
-						<NavBar visible={this.state.visible}>
-							<NavLink exact to={routes.home}>Home</NavLink>
-							<NavLink to={routes.chats}>Chats</NavLink>
-							<NavLink to={routes.signin}>SignIn</NavLink>
-							<NavLink to={routes.signup}>SignUp</NavLink>
-						</NavBar>
-					</BarCol>
-				</BarRow>
-			</Wrapper>
+				<BarCol>
+					<Flex>
+						<BarPoint color={`#E63E3E`}/>
+						<BarPoint color={`#EFBF14`}/>
+						<BarPoint color={`#38AD59`}/>
+					</Flex>
+				</BarCol>
+				<BarCol>
+					<NavBar visible={this.state.visible}>
+						<NavLink exact to={routes.home}>Home</NavLink>
+						<NavLink to={routes.chats}>Chats</NavLink>
+						<NavLink to={routes.signin}>SignIn</NavLink>
+						<NavLink to={routes.signup}>SignUp</NavLink>
+					</NavBar>
+				</BarCol>
+			</BarRow>
 		);
 	}
 }
